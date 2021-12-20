1e5f58985de3168247cc9100068864fc0f694cb3
AldairMerc <eduardomercado18@aragon.unam.mx>
2021-12-20 07:16:09 -0600

    Se agrega línea de tiempo en archivo lineatiempo.html, imagenes y archivo popup.js

---
+++ popup.js
@@ -0,0 +1,347 @@
+class popUps{
+	constructor (btnA, Ov, popup, btnC){
+		this.btnA = document.getElementById(btnA),
+		this.Ov = document.getElementById(Ov),
+		this.popup = document.getElementById(popup),
+		this.btnC = document.getElementById(btnC);
+	}
+
+}
+
+let popUp1 = new popUps('Boton1', 'Overlay1', 'Popup1', 'btn-cerrar-popup1');
+let popUp2 = new popUps('Boton2', 'Overlay2', 'Popup2', 'btn-cerrar-popup2');
+let popUp3 = new popUps('Boton3', 'Overlay3', 'Popup3', 'btn-cerrar-popup3');
+let popUp4 = new popUps('Boton4', 'Overlay4', 'Popup4', 'btn-cerrar-popup4');
+let popUp5 = new popUps('Boton5', 'Overlay5', 'Popup5', 'btn-cerrar-popup5');
+let popUp6 = new popUps('Boton6', 'Overlay6', 'Popup6', 'btn-cerrar-popup6');
+let popUp7 = new popUps('Boton7', 'Overlay7', 'Popup7', 'btn-cerrar-popup7');
+let popUp8 = new popUps('Boton8', 'Overlay8', 'Popup8', 'btn-cerrar-popup8');
+let popUp9 = new popUps('Boton9', 'Overlay9', 'Popup9', 'btn-cerrar-popup9');
+let popUp10 = new popUps('Boton10', 'Overlay10', 'Popup10', 'btn-cerrar-popup10');
+let popUp11 = new popUps('Boton11', 'Overlay11', 'Popup11', 'btn-cerrar-popup11');
+let popUp12 = new popUps('Boton12', 'Overlay12', 'Popup12', 'btn-cerrar-popup12');
+let popUp13 = new popUps('Boton13', 'Overlay13', 'Popup13', 'btn-cerrar-popup13');
+let popUp14 = new popUps('Boton14', 'Overlay14', 'Popup14', 'btn-cerrar-popup14');
+let popUp15 = new popUps('Boton15', 'Overlay15', 'Popup15', 'btn-cerrar-popup15');
+let popUp16 = new popUps('Boton16', 'Overlay16', 'Popup16', 'btn-cerrar-popup16');
+let popUp17 = new popUps('Boton17', 'Overlay17', 'Popup17', 'btn-cerrar-popup17');
+let popUp18 = new popUps('Boton18', 'Overlay18', 'Popup18', 'btn-cerrar-popup18');
+let popUp19 = new popUps('Boton19', 'Overlay19', 'Popup19', 'btn-cerrar-popup19');
+let popUp20 = new popUps('Boton20', 'Overlay20', 'Popup20', 'btn-cerrar-popup20');
+let popUp21 = new popUps('Boton21', 'Overlay21', 'Popup21', 'btn-cerrar-popup21');
+let popUp22 = new popUps('Boton22', 'Overlay22', 'Popup22', 'btn-cerrar-popup22');
+let popUp23 = new popUps('Boton23', 'Overlay23', 'Popup23', 'btn-cerrar-popup23');
+let popUp24 = new popUps('Boton24', 'Overlay24', 'Popup24', 'btn-cerrar-popup24');
+let popUp25 = new popUps('Boton25', 'Overlay25', 'Popup25', 'btn-cerrar-popup25');
+let popUp26 = new popUps('Boton26', 'Overlay26', 'Popup26', 'btn-cerrar-popup26');
+let popUp27 = new popUps('Boton27', 'Overlay27', 'Popup27', 'btn-cerrar-popup27');
+let popUp28 = new popUps('Boton28', 'Overlay28', 'Popup28', 'btn-cerrar-popup28');
+let popUp29 = new popUps('Boton29', 'Overlay29', 'Popup29', 'btn-cerrar-popup29');
+let popUp30 = new popUps('Boton30', 'Overlay30', 'Popup30', 'btn-cerrar-popup30');
+let popUp31 = new popUps('Boton31', 'Overlay31', 'Popup31', 'btn-cerrar-popup31');
+let popUp32 = new popUps('Boton32', 'Overlay32', 'Popup32', 'btn-cerrar-popup32');
+let popUp33 = new popUps('Boton33', 'Overlay33', 'Popup33', 'btn-cerrar-popup33');
+let popUp34 = new popUps('Boton34', 'Overlay34', 'Popup34', 'btn-cerrar-popup34');
+let popUp35 = new popUps('Boton35', 'Overlay35', 'Popup35', 'btn-cerrar-popup35');
+let popUp36 = new popUps('Boton36', 'Overlay36', 'Popup36', 'btn-cerrar-popup36');
+let popUp37 = new popUps('Boton37', 'Overlay37', 'Popup37', 'btn-cerrar-popup37');
+let popUp38 = new popUps('Boton38', 'Overlay38', 'Popup38', 'btn-cerrar-popup38');
+let popUp39 = new popUps('Boton39', 'Overlay39', 'Popup39', 'btn-cerrar-popup39');
+let popUp40 = new popUps('Boton40', 'Overlay40', 'Popup40', 'btn-cerrar-popup40');
+let popUp41 = new popUps('Boton41', 'Overlay41', 'Popup41', 'btn-cerrar-popup41');
+let popUp42 = new popUps('Boton42', 'Overlay42', 'Popup42', 'btn-cerrar-popup42');
+
+popUp1.btnA.addEventListener('click', function(){
+	popUp1.Ov.classList.add('active')
+} );
+popUp1.btnC.addEventListener('click', function(){
+	popUp1.Ov.classList.remove('active')
+} );
+
+popUp2.btnA.addEventListener('click', function(){
+	popUp2.Ov.classList.add('active')
+} );
+popUp2.btnC.addEventListener('click', function(){
+	popUp2.Ov.classList.remove('active')
+} );
+
+popUp3.btnA.addEventListener('click', function(){
+	popUp3.Ov.classList.add('active')
+} );
+popUp3.btnC.addEventListener('click', function(){
+	popUp3.Ov.classList.remove('active')
+} );
+
+popUp4.btnA.addEventListener('click', function(){
+	popUp4.Ov.classList.add('active')
+} );
+popUp4.btnC.addEventListener('click', function(){
+	popUp4.Ov.classList.remove('active')
+} );
+
+popUp5.btnA.addEventListener('click', function(){
+	popUp5.Ov.classList.add('active')
+} );
+popUp5.btnC.addEventListener('click', function(){
+	popUp5.Ov.classList.remove('active')
+} );
+
+popUp6.btnA.addEventListener('click', function(){
+	popUp6.Ov.classList.add('active')
+} );
+popUp6.btnC.addEventListener('click', function(){
+	popUp6.Ov.classList.remove('active')
+} );
+
+popUp7.btnA.addEventListener('click', function(){
+	popUp7.Ov.classList.add('active')
+} );
+popUp7.btnC.addEventListener('click', function(){
+	popUp7.Ov.classList.remove('active')
+} );
+
+popUp8.btnA.addEventListener('click', function(){
+	popUp8.Ov.classList.add('active')
+} );
+popUp8.btnC.addEventListener('click', function(){
+	popUp8.Ov.classList.remove('active')
+} );
+
+popUp9.btnA.addEventListener('click', function(){
+	popUp9.Ov.classList.add('active')
+} );
+popUp9.btnC.addEventListener('click', function(){
+	popUp9.Ov.classList.remove('active')
+} );
+
+popUp10.btnA.addEventListener('click', function(){
+	popUp10.Ov.classList.add('active')
+} );
+popUp10.btnC.addEventListener('click', function(){
+	popUp10.Ov.classList.remove('active')
+} );
+
+popUp11.btnA.addEventListener('click', function(){
+	popUp11.Ov.classList.add('active')
+} );
+popUp11.btnC.addEventListener('click', function(){
+	popUp11.Ov.classList.remove('active')
+} );
+
+popUp12.btnA.addEventListener('click', function(){
+	popUp12.Ov.classList.add('active')
+} );
+popUp12.btnC.addEventListener('click', function(){
+	popUp12.Ov.classList.remove('active')
+} );
+
+popUp13.btnA.addEventListener('click', function(){
+	popUp13.Ov.classList.add('active')
+} );
+popUp13.btnC.addEventListener('click', function(){
+	popUp13.Ov.classList.remove('active')
+} );
+
+popUp14.btnA.addEventListener('click', function(){
+	popUp14.Ov.classList.add('active')
+} );
+popUp14.btnC.addEventListener('click', function(){
+	popUp14.Ov.classList.remove('active')
+} );
+
+popUp15.btnA.addEventListener('click', function(){
+	popUp15.Ov.classList.add('active')
+} );
+popUp15.btnC.addEventListener('click', function(){
+	popUp15.Ov.classList.remove('active')
+} );
+
+popUp16.btnA.addEventListener('click', function(){
+	popUp16.Ov.classList.add('active')
+} );
+popUp16.btnC.addEventListener('click', function(){
+	popUp16.Ov.classList.remove('active')
+} );
+
+popUp17.btnA.addEventListener('click', function(){
+	popUp17.Ov.classList.add('active')
+} );
+popUp17.btnC.addEventListener('click', function(){
+	popUp17.Ov.classList.remove('active')
+} );
+
+popUp18.btnA.addEventListener('click', function(){
+	popUp18.Ov.classList.add('active')
+} );
+popUp18.btnC.addEventListener('click', function(){
+	popUp18.Ov.classList.remove('active')
+} );
+
+popUp19.btnA.addEventListener('click', function(){
+	popUp19.Ov.classList.add('active')
+} );
+popUp19.btnC.addEventListener('click', function(){
+	popUp19.Ov.classList.remove('active')
+} );
+
+popUp20.btnA.addEventListener('click', function(){
+	popUp20.Ov.classList.add('active')
+} );
+popUp20.btnC.addEventListener('click', function(){
+	popUp20.Ov.classList.remove('active')
+} );
+
+popUp21.btnA.addEventListener('click', function(){
+	popUp21.Ov.classList.add('active')
+} );
+popUp21.btnC.addEventListener('click', function(){
+	popUp21.Ov.classList.remove('active')
+} );
+
+popUp22.btnA.addEventListener('click', function(){
+	popUp22.Ov.classList.add('active')
+} );
+popUp22.btnC.addEventListener('click', function(){
+	popUp22.Ov.classList.remove('active')
+} );
+
+popUp23.btnA.addEventListener('click', function(){
+	popUp23.Ov.classList.add('active')
+} );
+popUp23.btnC.addEventListener('click', function(){
+	popUp23.Ov.classList.remove('active')
+} );
+
+popUp24.btnA.addEventListener('click', function(){
+	popUp24.Ov.classList.add('active')
+} );
+popUp24.btnC.addEventListener('click', function(){
+	popUp24.Ov.classList.remove('active')
+} );
+
+popUp25.btnA.addEventListener('click', function(){
+	popUp25.Ov.classList.add('active')
+} );
+popUp25.btnC.addEventListener('click', function(){
+	popUp25.Ov.classList.remove('active')
+} );
+
+popUp26.btnA.addEventListener('click', function(){
+	popUp26.Ov.classList.add('active')
+} );
+popUp26.btnC.addEventListener('click', function(){
+	popUp26.Ov.classList.remove('active')
+} );
+
+popUp27.btnA.addEventListener('click', function(){
+	popUp27.Ov.classList.add('active')
+} );
+popUp27.btnC.addEventListener('click', function(){
+	popUp27.Ov.classList.remove('active')
+} );
+
+popUp28.btnA.addEventListener('click', function(){
+	popUp28.Ov.classList.add('active')
+} );
+popUp28.btnC.addEventListener('click', function(){
+	popUp28.Ov.classList.remove('active')
+} );
+
+popUp29.btnA.addEventListener('click', function(){
+	popUp29.Ov.classList.add('active')
+} );
+popUp29.btnC.addEventListener('click', function(){
+	popUp29.Ov.classList.remove('active')
+} );
+
+popUp30.btnA.addEventListener('click', function(){
+	popUp30.Ov.classList.add('active')
+} );
+popUp30.btnC.addEventListener('click', function(){
+	popUp30.Ov.classList.remove('active')
+} );
+
+popUp31.btnA.addEventListener('click', function(){
+	popUp31.Ov.classList.add('active')
+} );
+popUp31.btnC.addEventListener('click', function(){
+	popUp31.Ov.classList.remove('active')
+} );
+
+popUp32.btnA.addEventListener('click', function(){
+	popUp32.Ov.classList.add('active')
+} );
+popUp32.btnC.addEventListener('click', function(){
+	popUp32.Ov.classList.remove('active')
+} );
+
+popUp33.btnA.addEventListener('click', function(){
+	popUp33.Ov.classList.add('active')
+} );
+popUp33.btnC.addEventListener('click', function(){
+	popUp33.Ov.classList.remove('active')
+} );
+
+popUp34.btnA.addEventListener('click', function(){
+	popUp34.Ov.classList.add('active')
+} );
+popUp34.btnC.addEventListener('click', function(){
+	popUp34.Ov.classList.remove('active')
+} );
+
+popUp35.btnA.addEventListener('click', function(){
+	popUp35.Ov.classList.add('active')
+} );
+popUp35.btnC.addEventListener('click', function(){
+	popUp35.Ov.classList.remove('active')
+} );
+
+popUp36.btnA.addEventListener('click', function(){
+	popUp36.Ov.classList.add('active')
+} );
+popUp36.btnC.addEventListener('click', function(){
+	popUp36.Ov.classList.remove('active')
+} );
+
+popUp37.btnA.addEventListener('click', function(){
+	popUp37.Ov.classList.add('active')
+} );
+popUp37.btnC.addEventListener('click', function(){
+	popUp37.Ov.classList.remove('active')
+} );
+
+popUp38.btnA.addEventListener('click', function(){
+	popUp38.Ov.classList.add('active')
+} );
+popUp38.btnC.addEventListener('click', function(){
+	popUp38.Ov.classList.remove('active')
+} );
+
+popUp39.btnA.addEventListener('click', function(){
+	popUp39.Ov.classList.add('active')
+} );
+popUp39.btnC.addEventListener('click', function(){
+	popUp39.Ov.classList.remove('active')
+} );
+
+popUp40.btnA.addEventListener('click', function(){
+	popUp40.Ov.classList.add('active')
+} );
+popUp40.btnC.addEventListener('click', function(){
+	popUp40.Ov.classList.remove('active')
+} );
+
+popUp41.btnA.addEventListener('click', function(){
+	popUp41.Ov.classList.add('active')
+} );
+popUp41.btnC.addEventListener('click', function(){
+	popUp41.Ov.classList.remove('active')
+} );
+
+popUp42.btnA.addEventListener('click', function(){
+	popUp42.Ov.classList.add('active')
+} );
+popUp42.btnC.addEventListener('click', function(){
+	popUp42.Ov.classList.remove('active')
+} );
+
